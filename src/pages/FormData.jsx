064c34cb543a7ca323c476676e445c8399a12b25
Ikkyuz <djdjv999@gmail.com>
2024-11-21 12:50:06 +0700

    Update file

--- src/pages/FormData.jsx
+++ src/pages/FormData.jsx
@@ -20,32 +20,34 @@ const FormData = () => {
   };
 
   return (
-    <>
-      <h1>แบบฟอร์มภาษี</h1>
-      <form onSubmit={submitForm}>
+    <div className="bg-gradient-to-br from-purple-200 to-purple-800 p-6 rounded-lg shadow-lg mx-auto">
+      <h1 className="text-center text-4xl font-bold my-5">แบบฟอร์มภาษี</h1>
+      <form onSubmit={submitForm} className="text-lg">
         <p>
           <label>เงินเดือน: </label>
-          <input type="text" name="salary" id="salary" onChange={handlaChange} />
+          <input type="text" name="salary" id="salary" onChange={handlaChange} className="w-full pl-1 border rounded-md"/>
         </p>
-        <p>
+        <p className="mt-1">
           <label>โบนัส: </label>
-          <input type="text" name="bonus" id="bonus" onChange={handlaChange} />
+          <input type="text" name="bonus" id="bonus" onChange={handlaChange} className="w-full pl-1 border rounded-md"/>
         </p>
-        <p>
+        <p className="mt-1">
           <label>ประกันสังคมต่อเดือน: </label>
-          <input type="text" name="ssecurity" id="ssecurity" onChange={handlaChange} />
+          <input type="text" name="ssecurity" id="ssecurity" onChange={handlaChange} className="w-full pl-1 border rounded-md"/>
         </p>
-        <p>
+        <p className="mt-1">
           <label>จำนวนบุตร: </label>
-          <input type="text" name="child" id="child" onChange={handlaChange} />
+          <input type="text" name="child" id="child" onChange={handlaChange} className="w-full pl-1 border rounded-md"/>
         </p>
-        <p>
+        <p className="mt-1">
           <label>ค่าเบี้ยประกันชีวิตต่อปี: </label>
-          <input type="text" name="slife" id="slife" onChange={handlaChange} />
+          <input type="text" name="slife" id="slife" onChange={handlaChange} className="w-full pl-1 border rounded-md"/>
         </p>
-        <button type="submit">Submit</button>
+        <div className="flex justify-end mt-3">
+          <button type="submit" className="bg-gray-100 text-purple-800 hover:bg-gradient-to-br from-purple-300 to-purple-500 hover:text-gray-100 font-bold rounded-lg shadow-lg p-1 px-2">Submit</button>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,18 +1,18 @@
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Results = () => {
   const location = useLocation();
   const { formData } = location.state;
 
   // ตัวแปร
-  let salary = formData.salary*1;
-  let bonus = formData.bonus*1;
+  let salary = formData.salary * 1;
+  let bonus = formData.bonus * 1;
   let ssecurity = formData.ssecurity;
   let child = formData.child;
-  let slife = formData.slife*1;
+  let slife = formData.slife * 1;
 
   // คำนวน
-  let salaryOfYear = (salary * 12) + bonus;
+  let salaryOfYear = salary * 12 + bonus;
   let expenses = salaryOfYear * 0.5 <= 100000 ? salaryOfYear * 0.5 : 100000;
   let security = ssecurity * 12;
   let childs = child <= 2 ? child : 2;
@@ -20,31 +20,49 @@ const Results = () => {
   let slack = 60000 + security + Childi + slife;
   let net = expenses - security - slack;
   let ptax = 0;
-  if (net <= 100000) ptax = 0
-  else if (net <= 300000) ptax = 0.05
-  else if (net <= 1000000) ptax = 0.10
+  if (net <= 100000) ptax = 0;
+  else if (net <= 300000) ptax = 0.05;
+  else if (net <= 1000000) ptax = 0.1;
   else ptax = 0.15;
+
   return (
-    <div>
-      <h1>ผลการคำนวน</h1>
-      <p>
-        <strong>เงินได้ทั้งปี: {salaryOfYear.toLocaleString()} บาท</strong>
-      </p>
-      <p>
-        <strong>ค่าให้จ่าย: {expenses.toLocaleString()} บาท</strong>
-      </p>
-      <p>
-        <strong>ค่ายลดหย่อน: {slack.toLocaleString()} บาท</strong>
-      </p>
-      <p>
-        <strong>เงินได้พึงประเมินสุทธิ: {net.toLocaleString()} บาท</strong>
-      </p>
-      <p>
-        <strong>อัตราภาษี: {(ptax*100).toLocaleString()} %</strong>
-      </p>
-      <p>
-        <strong>ภาษีที่ต้องชำระ: {net*ptax.toLocaleString()} บาท</strong>
-      </p>
+    <div className="bg-gradient-to-br from-purple-200 to-purple-800 p-6 rounded-lg shadow-lg mx-auto w-96 text-black">
+      <h1 className="text-center text-4xl font-bold my-5">ผลการคำนวน</h1>
+      <div className="space-y-4 text-lg">
+        <p className="flex justify-between">
+          <span>เงินได้ทั้งปี: </span>
+          <strong className="text-white">{salaryOfYear.toLocaleString()}</strong>
+          <span> บาท</span>
+        </p>
+        <p className="flex justify-between">
+          <span>ค่าให้จ่าย: </span>
+          <strong className="text-white">{expenses.toLocaleString()}</strong>
+          <span> บาท</span>
+        </p>
+        <p className="flex justify-between">
+          <span>ค่ายลดหย่อน: </span>
+          <strong className="text-white">{slack.toLocaleString()}</strong>
+          <span> บาท</span>
+        </p>
+        <p className="flex justify-between">
+          <span>เงินได้พึงประเมินสุทธิ: </span>
+          <strong className="text-white">{net.toLocaleString()}</strong>
+          <span> บาท</span>
+        </p>
+        <p className="flex justify-between">
+          <span>อัตราภาษี: </span>
+          <strong className="text-white">{(ptax * 100).toLocaleString()}</strong>
+          <span> %</span>
+        </p>
+        <p className="flex justify-between">
+          <span>ภาษีที่ต้องชำระ: </span>
+          <strong className="text-white">{net * ptax.toLocaleString()}</strong>
+          <span> บาท</span>
+        </p>
+        <div className="flex justify-end mt-4">
+          <NavLink to='/' className="bg-gray-100 text-purple-800 hover:bg-gradient-to-br from-purple-300 to-purple-500 hover:text-gray-100 font-bold rounded-lg shadow-lg p-1 px-2">Back</NavLink>
+        </div>
+      </div>
     </div>
   );
 };
